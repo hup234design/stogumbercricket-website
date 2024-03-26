4e051e0aca563846d208583c99cbfe5455da2650
HUP234 Design <37693783+hup234design@users.noreply.github.com>
2024-03-26 07:52:17 +0000

    update asset path

--- resources/views/livewire/blocks/downloads-block.blade.php
+++ resources/views/livewire/blocks/downloads-block.blade.php
@@ -4,7 +4,7 @@
         @foreach($downloads as $download)
             <div class="prose max-w-none py-8">
                 <!-- Title as a clickable link for download -->
-                <h3><a href="{{ asset($download->file) }}" download="{{ $download->title }}">{{ $download->title }}</a></h3>
+                <h3><a href="{{ asset('storage/'.$download->file) }}" download="{{ $download->title }}">{{ $download->title }}</a></h3>
                 <!-- Description -->
                 <p>{{ $download->description }}</p>
             </div>
